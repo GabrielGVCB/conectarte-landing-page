02a0a3ab61c3918dbb6dccf7aa2dce23500fd040
GabrielGVCB <gabriel.vinicius06.gb@gmail.com>
2024-02-16 16:16:13 -0300

    feat: :sparkles: upgrades landing page
    placing social media footer and hover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, UserComponent],
+  imports: [RouterOutlet, HomeComponent, UserComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
